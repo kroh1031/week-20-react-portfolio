9aa6a15c0f618361f5c800159f56c38abb8f4df4
Karen Roh <karenroh@hotmail.com>
2021-07-06 21:43:01 -0400

    able to open resume after importing pdf file

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,9 +1,14 @@
+import resume from "../../media/resume.a02ghs.pdf";
+
 const Resume = () => {
   return (
-    <div className="container">
+    <div className="container mt-3">
       <h1>Resume</h1>
       <div>
-        Download my <a href="">resume</a>
+        Download my{" "}
+        <a href={resume} target="_blank">
+          resume
+        </a>
       </div>
     </div>
   );
